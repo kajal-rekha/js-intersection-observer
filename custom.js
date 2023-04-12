44737cf01812d3a75614ab7b8504ef3ba00f69dc
kajal-2011 <kajalrekha888@gmail.com>
2023-04-13 02:52:30 +0600

    smooth scrolling added

--- custom.js
+++ custom.js
@@ -1,9 +1,12 @@
 "use strict";
 
+//  Elements
 const frame = document.querySelector(".frame");
 const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
+const navLinks = document.querySelector(".nav-links");
 
+// Slider
 let curSlide = 0;
 const lastSlide = 2;
 
@@ -29,5 +32,16 @@ nextBtn.addEventListener("click", () => {
   } else {
     curSlide++;
     transformFrame(curSlide);
+  }
+});
+
+// Smooth scrolling
+
+navLinks.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav-item")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
